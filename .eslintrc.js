30267af4dd13c6cac6db92d79900b6f24eafde88
zakicerdas <zakitoriq1@gmail.com>
2026-01-07 08:46:44 +0700

    kerangka awal project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    requireConfigFile: false,
+  },
 };
